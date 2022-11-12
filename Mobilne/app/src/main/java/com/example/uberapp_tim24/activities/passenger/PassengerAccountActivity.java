d0947d0a43365d9af9235cb782cda20efd1cc829
REX1389 <rrvlelcc@gmail.com>
2022-11-12 23:25:30 +0100

    Added PassengerAccountActivity and PassengerInboxActivity

---
+++ Mobilne/app/src/main/java/com/example/uberapp_tim24/activities/passenger/PassengerAccountActivity.java
@@ -0,0 +1,54 @@
+package com.example.uberapp_tim24.activities.passenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.uberapp_tim24.R;
+
+public class PassengerAccountActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passenger_account);
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart()
+    {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+    }
+}
