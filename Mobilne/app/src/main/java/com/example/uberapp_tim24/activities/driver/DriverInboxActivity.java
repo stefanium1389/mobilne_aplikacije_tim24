4dd950a76fc4217a1274b9bccfeee623f4ef7cd0
REX1389 <rrvlelcc@gmail.com>
2022-11-12 23:38:29 +0100

    Added PassengerRideHistoryActivity, DriverAccountActivity and DriverInboxActivity

---
+++ Mobilne/app/src/main/java/com/example/uberapp_tim24/activities/driver/DriverInboxActivity.java
@@ -0,0 +1,54 @@
+package com.example.uberapp_tim24.activities.driver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.uberapp_tim24.R;
+
+public class DriverInboxActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_inbox);
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart()
+    {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+    }
+}
