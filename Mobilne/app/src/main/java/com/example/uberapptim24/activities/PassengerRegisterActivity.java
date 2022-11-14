6b7349198feaf0b93d9eeb6fc059f38dccee003a
VladimirGolosin <vladimirgolosin1@gmail.com>
2022-11-14 17:08:00 +0100

    feat: register button leads to passenger main

--- Mobilne/app/src/main/java/com/example/uberapptim24/activities/PassengerRegisterActivity.java
+++ Mobilne/app/src/main/java/com/example/uberapptim24/activities/PassengerRegisterActivity.java
@@ -2,7 +2,10 @@ package com.example.uberapptim24.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.uberapptim24.R;
 
@@ -12,5 +15,14 @@ public class PassengerRegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passenger_register);
+
+        Button register = findViewById(R.id.passenger_register_button);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PassengerRegisterActivity.this, PassengerMainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
